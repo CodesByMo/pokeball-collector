08d7d7d21fae4103e98fa903df53ffa86e3f433f
CodesByMo <morganspencer162@gmail.com>
2019-05-22 10:20:26 -0700

    things

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+// need variables for crystals
+
+// generate random number 
+
+// track wins and losses
+
+// make crystal buttons clickable
+
+// keep track of total score when clicking crystal buttons 
+
